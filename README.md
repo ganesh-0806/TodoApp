28420e17836d368e3f5476aa27a22222f289d7a3
ganesh-0806 <ganeshregoti@gmail.com>
2021-10-22 14:27:48 +0530

    Project commit

---
+++ README.md
@@ -0,0 +1,11 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+This project is a simple TODO list using React JS
+Features:
+1. A text field to add the tasks. On clicking ENTER, the task is added to the list.
+2. To delete the task, just hover over the task on list and click
+
+To run the application, 
+1. Go to root location in terminal
+2. Run the following command
+    npmi; npm start;
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'Wake up early at 6:00AM'},
+      {id: 2, content: 'Work out for an hour'}
+    ]
+  };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos
+    })
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos
+    })
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
